37fdaa715c301d062af78c12123fb2c48d7d05a3
_jokl_ <joni_kl@web.de>
2023-07-21 21:44:09 +0200

    finished translation of the website into german

--- index copy.php
+++ index copy.php
@@ -8,7 +8,7 @@ JQuery documentation: https://api.jquery.com/jQuery/
 <head>
 	<meta charset="utf-8">
 	<title>Wetterstation Ismaning</title>
-	<link rel="stylesheet" type="text/css" href="index copy.css"/>
+	<link rel="stylesheet" type="text/css" href="index copy.css" />
 	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans" />
 	<script type="text/javascript" src="js/chart.umd.js"></script>
 	<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
@@ -22,59 +22,63 @@ JQuery documentation: https://api.jquery.com/jQuery/
 
 <body>
 	<div id="links" class="box-base">
-		<div class="top"><a href="http://www.waldorfschule-ismaning.de">Zurück zu der Website, von der Sie kommen</a></div>
-		<div class="middle"><a href="#">über diese Website</a></div>
-		<div class="bottom"><a href="#">Kontakt</a></div>
+		<div class="top"><a href="http://www.waldorfschule-ismaning.de">Zurück zur Waldorfschule</a></div>
+		<div class="middle"><a href="#">Der Quellcode</a></div>
+		<div class="bottom"><a href="#"></a></div>
 	</div>
 	<div id="control" class="box box-base">
 		<div class="title-bar top" onclick="switchControl('graph')">Liniendiagramm</div>
 		<div id="graph" class="control-content">
-					<div class="selection value">
-						<label>Select value</label>
-						<select id="selection1" onchange="v.update()">
-								<option value="temp">Temperature</option>
-								<option value="pressure">Pressure</option>
-								<option value="hum">Humidity</option>
-								<option value="windspeed">Wind speed</option>
-								<option value="rainrate">Rain rate</option>
-								<option value="uvindex">UV-index</option>
-						</select>
-					</div>
-					<div class="selection value">
-						<label>Optional second value</label>
-						<select id="selection2" onchange="v.update()">
-							<option value="none">None</option>
-								<option value="temp">Temperature</option>
-								<option value="pressure">Pressure</option>
-								<option value="hum">Humidity</option>
-								<option value="windspeed">Wind speed</option>
-								<option value="rainrate">Rain rate</option>
-								<option value="uvindex">UV-index</option>
-						</select>
-					</div>
-					<div class="selection newline">
-						<label>Start Date</label>
-						<div class="date"><input type="text" id="date-picker1" onchange="changeDate(0)" data-format="DD-MM-YYYY" data-template="D MMM YYYY" name="date"></div>
-					</div>
-					<div class="selection">
-						<label>End Date</label>
-						<div class="date"><input type="text" id="date-picker2" onchange="changeDate(1)" data-format="DD-MM-YYYY" data-template="D MMM YYYY" name="date"></div>
-					</div>
+			<div class="selection value">
+				<label>Erster Wert</label>
+				<select id="selection1" onchange="v.update()">
+					<option value="temp">Temperatur</option>
+					<option value="pressure">Druck</option>
+					<option value="hum">Luftfeuchtigkeit</option>
+					<option value="windspeed">Windgeschwindigkeit</option>
+					<option value="rainrate">Regenrate</option>
+					<option value="uvindex">UV-Index</option>
+				</select>
+			</div>
+			<div class="selection value">
+				<label>Zweiter Wert</label>
+				<select id="selection2" onchange="v.update()">
+					<option value="none">Nichts</option>
+					<option value="temp">Temperatur</option>
+					<option value="pressure">Druck</option>
+					<option value="hum">Luftfeuchtigkeit</option>
+					<option value="windspeed">Windgeschwindigkeit</option>
+					<option value="rainrate">Regenrate</option>
+					<option value="uvindex">UV-Index</option>
+				</select>
+			</div>
+			<div class="selection newline">
+				<label>Beginn</label>
+				<div class="date"><input type="text" id="date-picker1" onchange="changeDate(0)" data-format="DD-MM-YYYY" data-template="D MMM YYYY" name="date"></div>
+			</div>
+			<div class="selection">
+				<label>Ende</label>
+				<div class="date"><input type="text" id="date-picker2" onchange="changeDate(1)" data-format="DD-MM-YYYY" data-template="D MMM YYYY" name="date"></div>
+			</div>
 		</div>
-		<div id="table-title" class="title-bar" onclick="switchControl('table')">Table</div>
+		<div id="table-title" class="title-bar" onclick="switchControl('table')">Tabelle</div>
 		<div id="table" class="control-content" style="display: none">
-					<div class="selection">
-						<label>Interval</label>
-						<select id="interval" onchange="v.update()()"></select>
-					</div>
-					<div class="selection">
-						<label>Select value</label>
-						<select id="selection1" onchange="v.update()()"></select>
-					</div>
-					<div class="selection">
-						<label>Optional second value</label>
-						<select id="selection2" onchange="v.update()()"></select>
-					</div>
+			<div class="construction-site">
+				Ups, hier fehlt noch was...
+			</div>
+			<div class="selection value">
+				<label>Interval</label>
+				<select id="interval" onchange="v.update()"></select>
+			</div>
+			<div class="selection value">
+				<label>Erster Wert</label>
+				<select id="selection1" onchange="v.update()"></select>
+			</div>
+			<div class="selection value">
+				<label>Optionaler zweiter Wert</label>
+				<select id="selection2" onchange="v.update()"></select>
+			</div>
+			<!-- there should be possibilities to select as many values as one wants, not just 2 -->
 		</div>
 		<div class="title-bar bottom" style="display: none"></div>
 	</div>
